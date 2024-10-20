23770d7b97a8f93c2f29096931ea9341e6e55bbd
imran hassan <hassanimran114@gmail.com>
2024-10-20 14:17:18 +0530

    fimran

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
+const port =5000;
 require('dotenv').config()
 
 // middleware
@@ -19,6 +19,10 @@ const orderRoutes = require("./src/orders/order.route")
 const userRoutes =  require("./src/users/user.route")
 const adminRoutes = require("./src/stats/admin.stats")
 
+app.get("/", (req, res) => {
+  res.send("Book Store Server is running!");
+});
+
 app.use("/api/books", bookRoutes)
 app.use("/api/orders", orderRoutes)
 app.use("/api/auth", userRoutes)
@@ -26,9 +30,7 @@ app.use("/api/admin", adminRoutes)
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
-  app.get("/", (req, res) => {
-    res.send("Book Store Server is running!");
-  });
+ 
 }
 
 main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
